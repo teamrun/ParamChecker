7a662381d36f7c4761288f25f143a8799e7a78eb
陈亮亮 <chenliangliang@souche.com>
2015-06-26 16:18:05 +0800

    getValue的bug, 已修复

--- checker/getValue.js
+++ checker/getValue.js
@@ -17,7 +17,11 @@ var GET_FUNC = {
     },
     _default: function(key){
         return function(){
-            return this.params[key] ||  this.params[key] || this.request.body.fields[key];
+            // console.log('key: ', key);
+            // console.log(this.params[key]);
+            // console.log(this.query[key]);
+            // console.log(this.request.body.fields[key]);
+            return this.params[key] ||  this.query[key] || this.request.body.fields[key];
         }
     }
 }
