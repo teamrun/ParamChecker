7b2fd7b05a31d80544d150ccf1f166e1ede519ac
陈亮亮 <chenliangliang@souche.com>
2015-08-11 23:31:27 +0800

    去掉取值-校验类型 的高阶函数
    用最基本的 读取配置-调用方法 来实现

--- checker/getValue.js
+++ checker/getValue.js
@@ -1,28 +1,24 @@
+// koa版本的取参数方法
+// koa依赖于context, 所以要保证去参数的context要正确
+
 // requires: koa-router, koa-better-body
 var GET_FUNC = {
     fromUrl: function(key){
-        return function(){
-            return this.params[key];
-        }
+        // console.log(this);
+        return this.params[key];
     },
     fromQuery: function(key){
-        return function(){
-            return this.query[key];
-        }
+        return this.query[key];
     },
     fromBody: function(key){
-        return function(){
-            return this.request.body.fields[key];
-        }
+        return this.request.body.fields[key];
     },
     _default: function(key){
-        return function(){
-            // console.log('key: ', key);
-            // console.log(this.params[key]);
-            // console.log(this.query[key]);
-            // console.log(this.request.body.fields[key]);
-            return this.params[key] ||  this.query[key] || this.request.body.fields[key];
-        }
+        // console.log('key: ', key);
+        // console.log(this.params[key]);
+        // console.log(this.query[key]);
+        // console.log(this.request.body.fields[key]);
+        return this.params[key] ||  this.query[key] || this.request.body.fields[key];
     }
 }
 
